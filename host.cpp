9629cb7858fd0fa1a2e78cfddb4e7392516ac0da
ychy61 <ychy010601@gmail.com>
2024-06-20 20:26:29 +0900

    week1 fix host.cpp

--- host.cpp
+++ host.cpp
@@ -6,54 +6,40 @@
 void Host::initialize()
 {
 }
-
 void Host::send(Packet *packet)
 {
       std::string from = packet->srcAddress().toString();
       std::string to = packet->destAddress().toString();
       std::string dataLength = std::to_string(packet->dataString().size());
-
       if (address_ == packet->srcAddress())
       {
             std::cout << "Host #" << id() << ": sending packet (from: " << from << ", to: " << to << ", " << dataLength << " bytes)" << std::endl;
-
-            std::mt19937 rng(std::random_device{}());
-            std::uniform_int_distribution<std::size_t> dist(0, linkTable().size() - 1);
-            linkTable()[dist(rng)]->onReceive(this, packet);
-      }
-      else if (address_ == packet->destAddress())
-      {
-            Service *service = Host::getService(packet->destPort());
-            std::cout << "Host #" << id() << ": received packet, destination port: " << packet->destPort() << std::endl;
-            if (service == nullptr)
+            size_t linkTableSize = linkTable().size();
+            if (linkTableSize > 0)
             {
-                  std::cout << "Host #" << id() << ": no service for packet (from: " << from << ", to: " << to << ", " << dataLength << " bytes)" << std::endl;
-                  delete packet;
-                  return;
+                  linkTable()[rand() % linkTableSize]->onReceive(this,packet);
             }
-            service->onReceive(packet);
       }
 }
 
 void Host::onReceive(Packet *packet)
 {
+      std::string from = packet->srcAddress().toString();
+      std::string to = packet->destAddress().toString();
+      std::string dataLength = std::to_string(packet->dataString().size());
       if (address_ == packet->destAddress())
       {
-            Service *service = getService(packet->destPort());
-            if (service != nullptr)
+            std::cout << "Host #" << id() << ": received packet, destination port: " << packet->destPort() << std::endl;
+            Service *service = Host::getService(packet->destPort());
+            if (service == nullptr)
             {
-                  service->onReceive(packet);
+                  std::cout << "Host #" << id() << ": no service for packet (from: " << from << ", to: " << to << ", " << dataLength << " bytes)" << std::endl;
+                  delete packet;
             }
             else
             {
-                  std::cerr << "Host #" << id() << ": No service available on port " << packet->destPort() << std::endl;
-                  delete packet;
+                  service->onReceive(packet);
             }
-      }
-      else
-      {
-            std::cerr << "Host #" << id() << ": Received packet with incorrect destination address." << std::endl;
-            delete packet;
       }
 }
 
